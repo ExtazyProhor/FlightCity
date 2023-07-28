d004a66149eac9e69aa82655561d716474d0789c
ExtazyProhor <prohor2568@gmail.com>
2023-07-28 22:02:01 +0300

    добавлен класс Languages, заместо pref-ов для локализации текста

---
+++ core/src/com/mygdx/game/Languages.java
@@ -0,0 +1,47 @@
+package com.mygdx.game;
+
+public abstract class Languages {
+
+//    english - 0
+//	  russian - 1
+//	  belorussian - 2
+
+    /*
+    public static final String[]  = {
+            "",
+            "",
+            ""
+    };
+    */
+
+    public static final String[] play = {
+            "play",
+            "играть",
+            "гуляць"
+    };
+    public static final String[] settings = {
+            "settings",
+            "настройки",
+            "наладкі"
+    };
+    public static final String[] sound = {
+            "sounds",
+            "звуки",
+            "гукі"
+    };
+    public static final String[] music = {
+            "music",
+            "музыка",
+            "музыка"
+    };
+    public static final String[] language = {
+            "language",
+            "язык",
+            "мова"
+    };
+    public static final String[] save = {
+            "save",
+            "сохранить",
+            "захаваць"
+    };
+}
